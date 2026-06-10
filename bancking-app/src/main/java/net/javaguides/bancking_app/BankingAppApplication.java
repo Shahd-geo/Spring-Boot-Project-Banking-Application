e38cd997625e4417b90c8da011c58f4e78e5c51a
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-10 14:18:00 +0400

    create entity package

--- bancking-app/src/main/java/net/javaguides/bancking_app/BankingAppApplication.java
+++ bancking-app/src/main/java/net/javaguides/bancking_app/BankingAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BanckingAppApplication {
+public class BankingAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BanckingAppApplication.class, args);
+		SpringApplication.run(BankingAppApplication.class, args);
 	}
 
 }
