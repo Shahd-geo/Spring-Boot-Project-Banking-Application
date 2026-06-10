91c42c065549ecf2037073fdff8cb41946eea3ba
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-10 15:36:10 +0400

    ADD PostMapping FOR ADDING CUSTOMER

--- bancking-app/src/main/java/net/javaguides/bancking_app/Controllers/CustomerController.java
+++ bancking-app/src/main/java/net/javaguides/bancking_app/Controllers/CustomerController.java
@@ -1,9 +1,11 @@
 package net.javaguides.bancking_app.Controllers;
 
+import net.javaguides.bancking_app.entity.Customer;
 import net.javaguides.bancking_app.repository.CustomerRepository;
 import net.javaguides.bancking_app.services.CustomerServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
     @Autowired
     CustomerServices customerServices;
+    @PostMapping("add")
+    public Customer addCustomer(@RequestBody Customer customer){
+        return customerServices.addCustomer(customer);
+    }
 
 }
 
