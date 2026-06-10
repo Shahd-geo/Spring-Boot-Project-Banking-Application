98e18ab95f59072837a4a5b64470568a7685cc4f
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-10 15:32:36 +0400

    ADD RequestMapping /customers

--- bancking-app/src/main/java/net/javaguides/bancking_app/Controllers/CustomerController.java
+++ bancking-app/src/main/java/net/javaguides/bancking_app/Controllers/CustomerController.java
@@ -2,9 +2,11 @@ package net.javaguides.bancking_app.Controllers;
 
 import net.javaguides.bancking_app.repository.CustomerRepository;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/customers")
 
 public class CustomerController {
 }
