5de6662c5aa47c13355b90ed09efd441d4cad324
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-10 14:23:00 +0400

    create entity notation

--- bancking-app/src/main/java/net/javaguides/bancking_app/entity/Customer.java
+++ bancking-app/src/main/java/net/javaguides/bancking_app/entity/Customer.java
@@ -1,4 +1,9 @@
 package net.javaguides.bancking_app.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customers")
 public class Customer {
 }
