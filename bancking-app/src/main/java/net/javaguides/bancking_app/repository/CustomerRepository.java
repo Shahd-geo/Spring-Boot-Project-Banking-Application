4021a7e85b4ccd839a249252112816a37c86e51f
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-10 15:31:10 +0400

    mplement business logic for customer management

--- bancking-app/src/main/java/net/javaguides/bancking_app/repository/CustomerRepository.java
+++ bancking-app/src/main/java/net/javaguides/bancking_app/repository/CustomerRepository.java
@@ -2,6 +2,11 @@ package net.javaguides.bancking_app.repository;
 
 import net.javaguides.bancking_app.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+    List<Customer> findByCustomerName(String customerName);
+
 }
--- bancking-app/src/main/java/net/javaguides/bancking_app/services/CustomerServices.java
+++ bancking-app/src/main/java/net/javaguides/bancking_app/services/CustomerServices.java
@@ -5,13 +5,58 @@ import net.javaguides.bancking_app.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CustomerServices {
     @Autowired
     private CustomerRepository customerRepository;
 
-
+    //Add Customer
     public Customer addCustomer(Customer customer){
         return customerRepository.save(customer);
     }
+    //Get All Customers - FIND ALL
+    public List<Customer> getAllCustomers(){
+        return customerRepository.findAll();
+    }
+    //Get Customer By ID-FIND BY ID
+    public Customer getCustomerById(Integer id){
+        return customerRepository.findById(id).orElse(null);
+    }
+
+    //Find Customer by Name
+
+    public List<Customer> getCustomerByName(String customerName){
+        return customerRepository.findByCustomerName(customerName);
+    }
+    //Update Customer
+    public Customer updateCustomer(Integer id , Customer customer){
+
+        Customer existingCustomer = customerRepository.findById(id).orElse(null);
+
+        if(existingCustomer != null){
+
+            existingCustomer.setCustomerName(customer.getCustomerName());
+            existingCustomer.setAccountNumber(customer.getAccountNumber());
+            existingCustomer.setBalance(customer.getBalance());
+            existingCustomer.setEmail(customer.getEmail());
+            existingCustomer.setPhoneNumber(customer.getPhoneNumber());
+
+            return customerRepository.save(existingCustomer);
+        }
+
+        return null;
+    }
+    //Delete Customer
+    public Boolean deleteCustomer(Integer id){
+        Customer customer = customerRepository.findById(id).orElse(null);
+        if(customer != null){
+            customerRepository.deleteById(id);
+            return true;
+        }
+
+        return false;
+    }
+
 }
