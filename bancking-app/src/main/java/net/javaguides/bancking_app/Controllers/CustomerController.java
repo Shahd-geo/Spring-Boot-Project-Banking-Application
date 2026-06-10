cfc9c74d32b2764a2756b863b9bdda7de777c4ec
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-10 15:36:46 +0400

    ADD GetMapping TO FIND ALL CUSTOMER

--- bancking-app/src/main/java/net/javaguides/bancking_app/Controllers/CustomerController.java
+++ bancking-app/src/main/java/net/javaguides/bancking_app/Controllers/CustomerController.java
@@ -4,10 +4,9 @@ import net.javaguides.bancking_app.entity.Customer;
 import net.javaguides.bancking_app.repository.CustomerRepository;
 import net.javaguides.bancking_app.services.CustomerServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/customers")
@@ -19,6 +18,11 @@ public class CustomerController {
     public Customer addCustomer(@RequestBody Customer customer){
         return customerServices.addCustomer(customer);
     }
+    @GetMapping("getAll")
+    public List<Customer> getAllCustomers(){
+        return customerServices.getAllCustomers();
+    }
+
 
 }
 
